f79ad9161cb490c59ea814069fafc25ebbe69e64
BigCircle <zhuceafter2015@163.com>
2018-04-09 04:37:21 -0400

    test file is ready

---
+++ knn_predictprice/knn_predictprice/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  knn_predictprice
+//
+//  Created by Dayuan•Neil Tan on 4/9/18.
+//  Copyright © 2018 Dayuan•Neil Tan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
